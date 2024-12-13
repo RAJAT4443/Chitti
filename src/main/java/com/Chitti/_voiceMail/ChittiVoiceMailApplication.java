38652567935b0cf3a4e0a7fd22800236b62d0af3
Rajat Singh <rajat.singh@blackngreen.com>
2024-12-13 20:28:49 +0530

    initial commit

---
+++ src/main/java/com/Chitti/_voiceMail/ChittiVoiceMailApplication.java
@@ -0,0 +1,13 @@
+package com.Chitti._voiceMail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChittiVoiceMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChittiVoiceMailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Chitti/_voiceMail/ChittiVoiceMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Chitti._voiceMail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChittiVoiceMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
